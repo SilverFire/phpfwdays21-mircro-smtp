f3db112ff55ae6bad38c5cc10b3dd6f24c1ff8f0
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2021-09-05 08:30:48 +0300

    Inited

---
+++ src/Builder/BuilderInterface.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace MicroMailer\Builder;
+
+interface BuilderInterface
+{
+}
---
+++ tests/Unit/Builder/BuilderTest.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace MicroMailer\tests\Unit\Builder;
+
+use PHPUnit\Framework\TestCase;
+
+class BuilderTest extends TestCase
+{
+
+}
